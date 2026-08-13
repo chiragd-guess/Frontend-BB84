0c8de1ee995e3a2ee6c663fffaa7cbad6e7c5cd3
Chirag-sudo2333 <chiragdahal0013@gmail.com>
2026-08-13 13:24:49 +0545

    Update frontend and project files

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,5 +1,5 @@
 import StatusPill from "../StatusPill/StatusPill";
-import logo from "../../assets/logon.png";"
+import logo from "../../assets/logon.png"
 
 export default function TopBar({ simulation, onReset, drawerOpen, onToggleDrawer }) {
   const status = simulation?.status ?? "idle";
